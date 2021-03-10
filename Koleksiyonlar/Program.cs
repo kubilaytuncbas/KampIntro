7e7b94dcb00a7055d77fc47dd500f1920070308f
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-03-11 00:08:30 +0300

    3..ders

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "engin", "murat", "kubilay", "halil" };
+            //console.writeline(isimler[0]);
+            //console.writeline(isimler[1]);
+            //console.writeline(isimler[2]);
+            //console.writeline(isimler[3]);
+            List<string> isimler2 = new List<string>();
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2[4] = "ali";
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+
+
+
+
+
+
+        }
+    }
+}
